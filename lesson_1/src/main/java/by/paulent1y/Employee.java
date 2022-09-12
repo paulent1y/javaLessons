ec504177f26c762dbf1762c4881b4301072438e0
paulent1y <gispavel2017@gmail.com>
2022-09-13 01:39:50 +0200

    done

---
+++ lesson_1/src/main/java/by/paulent1y/Employee.java
@@ -0,0 +1,30 @@
+package by.paulent1y;
+
+public class Employee {
+    private String name;
+    private int age;
+    private String position;
+    private String email;
+    private String phone;
+    private int salary;
+
+    public Employee(String name, int age, String position, String email,  String phone, int salary)  {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+    }
+
+    public void getInfo() {
+        System.out.print("Employee " + name + " " + age + " years old");
+        System.out.print(". Works as " + position + " with salary " + salary);
+        System.out.print(". Contacts: "  + email + ", +" + phone);
+        System.out.println();
+    }
+
+    public boolean isOlder(int n) {
+        return (age>n);
+    }
+}
