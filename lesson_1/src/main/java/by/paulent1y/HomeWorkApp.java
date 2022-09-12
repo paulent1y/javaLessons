088c305407e48df7a0398c2e517ef5712ef2d4cd
paulent1y <gispavel2017@gmail.com>
2022-09-13 00:18:56 +0200

    empty variant for branching from

--- lesson_1/src/main/java/by/paulent1y/HomeWorkApp.java
+++ lesson_1/src/main/java/by/paulent1y/HomeWorkApp.java
@@ -2,34 +2,6 @@ package by.paulent1y;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
     }
 
-    public static void printThreeWords(){
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
-    public static void checkSumSign(){
-        int a=10,b=20;
-        if (a+b>=0) System.out.println("Сумма положительная");
-        else System.out.println("Сумма отрицательная");
-    }
-
-    public static void printColor(){
-        int value = 10;
-        if (value<=0) System.out.println("Краный");
-        else if (value>0 && value<=100) System.out.println("Желтый");
-        else System.out.println("Зеленый");
-    }
-
-    public static void compareNumbers() {
-        int a=10, b=20;
-        if (a>=b) System.out.println("a >= b");
-        else System.out.println("a < b");
-    }
 }
