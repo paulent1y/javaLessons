4063633976c74f962d400194eb2a51a2be772e7d
paulent1y <gispavel2017@gmail.com>
2022-09-25 10:17:28 +0200

    started

--- lesson_1/src/main/java/by/paulent1y/HomeWorkApp.java
+++ lesson_1/src/main/java/by/paulent1y/HomeWorkApp.java
@@ -2,7 +2,9 @@ package by.paulent1y;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-
+        GenericBox<Double, Integer> genStr = new GenericBox<>(40.4, 50);
+        genStr.showType();
+        System.out.println("genStr.getObject():" + genStr.getObj1());
     }
 
 }
