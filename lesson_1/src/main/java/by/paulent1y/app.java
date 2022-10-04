3dfda2c9e2db81d2d94990c97df19ad5ed42ba0e
paulent1y <gispavel2017@gmail.com>
2022-10-04 19:38:22 +0200

    task done

--- lesson_1/src/main/java/by/paulent1y/app.java
+++ lesson_1/src/main/java/by/paulent1y/app.java
@@ -7,20 +7,21 @@ import java.util.*;
 public class app {
 
     public static void main(String[] args) throws Exception {
-        String[] headers = {"History", "Math", "Chemistry", "Physics"};
-
-        AppData ad = new AppData(headers);
-        ad.buildRandomData(headers.length,10);
-        ad.writeTo("generated.txt");
-        System.out.println("Generated data");
-        System.out.println(ad);
-
 
         AppData ad2 = new AppData();
         ad2.readFrom("generated.txt");
         System.out.println("Read data from file");
         System.out.println(ad2);
         ad2.writeTo("fromRead.txt");
+
+        String[] headers = {"History", "Math", "Chemistry", "Physics"};
+        AppData ad = new AppData(headers);
+        ad.buildRandomData(headers.length,10);
+        ad.writeTo("generated.txt");
+        System.out.println("Written generated data to file");
+        System.out.println(ad);
+
+
     }
 
 
