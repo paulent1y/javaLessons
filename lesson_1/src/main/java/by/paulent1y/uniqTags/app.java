ecb1d055123445851afb912ebeb7c9dff625171e
paulent1y <gispavel2017@gmail.com>
2022-10-08 00:01:18 +0200

    output cleared

--- lesson_1/src/main/java/by/paulent1y/uniqTags/app.java
+++ lesson_1/src/main/java/by/paulent1y/uniqTags/app.java
@@ -2,25 +2,43 @@ package by.paulent1y.uniqTags;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.Map.Entry;
 
 public class app {
     public static void main(String[] args) {
-        Set<String> tags = new HashSet<>();
+        List<String> allTags = new ArrayList<>();
         try(BufferedReader reader = new BufferedReader(new FileReader("lmf.txt"))){
-            String l = new String();
+            String l;
             while ((l= reader.readLine()) != null) {
-                tags.addAll(Arrays.asList(l.split(" ")));
-                System.out.println(l);
+                allTags.addAll(Arrays.asList(l.split(" ")));
             }
         }
         catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
 
-        System.out.println(tags);
+        Set<String> tags = new HashSet<>(allTags);
+        Map<String, Integer> tagsRepeated = new HashMap<>();
+        for (String s:tags){
+            tagsRepeated.put(s, Collections.frequency(allTags, s));
+        }
+        Map<String, Integer> sortedTags= sortByValue(tagsRepeated);
+        for (Entry<String, Integer> pair: sortedTags.entrySet()) {
+            if (pair.getValue()>1)
+            System.out.println(pair.getKey());
+        }
+    }
+
+    public static <K, V extends Integer> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Entry.comparingByValue(Comparator.reverseOrder()));
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
     }
 
 }
